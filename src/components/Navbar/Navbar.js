707430f74fe8b30f190eae5ebb5edd88db400b2e
Andres Icedo <andresicedo24@gmail.com>
2021-08-30 13:03:53 -0500

    completed navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import './Navbar.css';
 import Logo from '../../assets/images/kloursLogo.png'
+import PaintRoller from '../../assets/images/PaintRoller.png'
+import Pricing from '../../assets/images/Pricing.png'
+import About from '../../assets/images/About.png'
+import ContactUs from '../../assets/images/ContactUs.png'
 
 export default function Navbar() {
     return (
         <div className='navbarContainer'>
             <div className='navbarLeft'>
-                <img alt="" src={Logo} style={{width: '150px', height: 'auto'}} />
+                <img alt="companyLogo" src={Logo} style={{width: '150px', height: 'auto'}} />
             </div>
             <div className='navbarRight'>
-                <p>Home</p>
-                <p>Pricing</p>
-                <p>Contact Us</p>
+                <p>
+                    <button style={{background: 'none', border: 'none'}}>
+                        <img alt="bucketImage" src={PaintRoller} style={{width: '40px'}}/>
+                    </button>
+                </p>
+                <p>
+                    <button style={{background: 'none', border: 'none'}}>
+                        <img alt="pricingImage" src={Pricing} style={{width: '40px'}}/>
+                    </button>
+                </p>
+                <p>
+                    <button style={{background: 'none', border: 'none'}}>
+                        <img alt="HeartImage" src={About} style={{width: '40px'}}/>
+                    </button>
+                </p>
+                <p>
+                    <button style={{background: 'none', border: 'none'}}>
+                        <img alt="ContactIcon" src={ContactUs} style={{width: '40px'}}/>
+                    </button>
+                </p>
             </div>
         </div>
     )
