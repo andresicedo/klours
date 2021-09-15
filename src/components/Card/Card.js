cfeb4a23503ffa2b987323e71aff9f8d88934dd8
Andres Icedo <andresicedo24@gmail.com>
2021-09-14 19:08:46 -0500

    updated ideas cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Card.css'
 
-export default function Card({room}) {
+export default function Card({room, cardImg, cardText}) {
     return (
         <div className="card" style={{ width: '18rem' }}>
-            <img src="..." className="card-img-top" alt="..." />
+            <img src={cardImg} className="card-img-top" alt="home interior" style={{width: '100%', height: '250px'}} />
             <div className="card-body">
                 <h5 className="card-title">{room}</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button className="btn btn-primary">Go somewhere</button>
+                <p className="card-text" style={{height: '60%'}}>{cardText}</p>
+                <button className="btn btn-primary">View Ideas</button>
             </div>
         </div>
     )
--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -2,8 +2,17 @@ import React from 'react'
 import Card from '../Card/Card'
 import './Ideas.css'
 import ProjectIdeasImg from '../../assets/images/ProjectIdeas.jpg'
+import Bedroom from '../../assets/images/Bedroom.jpg';
+import Kitchen from '../../assets/images/Kitchen.jpg';
+import Outdoor from '../../assets/images/Outdoor.jpg';
+
 
 export default function Ideas() {
+    let kitchenIdeas = 'Rethink the way you see your cabinets with these easy-to-follow painting projects.'
+    let outdoorIdeas = "Brighten up your home's exterior with projects that boost curb appeal and give your space a personalized look."
+    let bedroomIdeas = 'Painting a room offers an inexpensive way to refresh its appearance. A new coat of paint can brighten up a small room or bring warmth to large spaces.'
+
+
     return (
         <div>
             <div className="ideasTitle" 
@@ -14,9 +23,9 @@ export default function Ideas() {
                 <p>Project Ideas</p>
             </div>
             <div className="ideaCards">
-                <Card room={'Bedroom'}/>
-                <Card room={'Kitchen'}/>
-                <Card room={'Outdoor'}/>
+                <Card room={'Bedroom'} cardImg={Bedroom} cardText={bedroomIdeas}/>
+                <Card room={'Kitchen'} cardImg={Kitchen} cardText={kitchenIdeas}/>
+                <Card room={'Outdoor'} cardImg={Outdoor} cardText={outdoorIdeas}/>
             </div>
         </div>
     )
