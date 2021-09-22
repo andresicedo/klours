e407c0a419bc8a2c91d6e78d4d30142072264a80
Andres Icedo <andresicedo24@gmail.com>
2021-09-22 18:07:21 -0500

    added carousel to products section

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,17 +4,28 @@ import Card from '../Card/Card';
 import HomePaint from '../../assets/images/HomePaint.jpg';
 import StainedWood from '../../assets/images/StainedWood.jpg';
 import ConcreteFloor from '../../assets/images/ConcreteFloor.jpg';
+import Carousel from '../Carousel/Carousel';
 
 export default function Products() {
+
+    let carouselDetails = {
+        paintingProducts: ['Painting Products', HomePaint, 'Test'],
+        stainingProducts: ['Staining Products', StainedWood, 'Test'],
+        concreteProducts: ['Concrete Products', ConcreteFloor, 'Test'],
+    }
+
     return (
         <div>
             <div>
                 <p>Products Component</p>
             </div>
+
+           <Carousel carouselDetails={carouselDetails}/>
+
             <div className='productsCards'>
-                <Card type={'Painting Products'} cardImg={HomePaint} cardText={'Test'}/>
-                <Card type={'Staining Products'} cardImg={StainedWood} cardText={'Test'}/>
-                <Card type={'Concrete Products'} cardImg={ConcreteFloor} cardText={'Test'}/>
+                <Card type={'Painting Products'} cardImg={HomePaint} cardText={'Test'} />
+                <Card type={'Staining Products'} cardImg={StainedWood} cardText={'Test'} />
+                <Card type={'Concrete Products'} cardImg={ConcreteFloor} cardText={'Test'} />
             </div>
         </div>
     )
