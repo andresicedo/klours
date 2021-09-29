0d9e2e753754c66a650be2727ff8bc6827008f54
Andres Icedo <andresicedo24@gmail.com>
2021-09-28 21:27:36 -0500

    updated color palette options

--- src/components/ColorSelection/ColorSelection.js
+++ src/components/ColorSelection/ColorSelection.js
@@ -4,14 +4,29 @@ import Square from '../Square/Square';
 import './ColorSelection.css';
 
 export default function ColorSelection() {
+
+    let getWidth = (idx) => {
+        return (idx === 0 ? 120 : 75);
+    }
+    
+    let colorOptions = [
+        ['435F62', 'B2CBC8', 'C0C8BB', 'B6BDAD', 'FFFFFF'],
+        ['D4774C', 'D2C2A9', 'F6F2E6', '6C6C6A', '497E7E'],
+        ['BCC5CD', 'BBBDB8', 'EADECE', '8F8792', 'FFFFFF'],
+        ['A0B3BA', '6A7881', 'D7D4CB', 'DCDEDA', 'FFFFFF'],
+        ['EDE1CE', '73645D', 'B9B2AC', 'C0C1BC', 'FFFFFF']
+    ];
+
+    
+
     return (
         <div className="colorSelectionContainer">
             <div className="selectedPallette">
-                <Rectangle w={75} h={200} m={10}/>
-                <Rectangle w={75} h={200} m={10}/>
-                <Rectangle w={75} h={200} m={10}/>
-                <Rectangle w={75} h={200} m={10}/>
-                <Rectangle w={75} h={200} m={10}/>
+                {
+                    colorOptions[0].map((option) => (
+                        <Rectangle key={option} w={getWidth(colorOptions[0].indexOf(option))} h={200} m={10} bgColor={option}/>
+                    ))
+                }
             </div>
             <div className="paletteOptions">
                 <Square w={45} h={45} m={6}/>
--- src/components/Rectangle/Rectangle.js
+++ src/components/Rectangle/Rectangle.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Rectangle.css';
 
-export default function Rectangle({w, h, m}) {
+export default function Rectangle({w, h, m, bgColor}) {
     return (
-        <div className="rectangle" style={{width: `${w}px`, height: `${h}px`, marginRight: `${m}px`}}>
+        
+        <div className="rectangle" style={{width: `${w}px`, height: `${h}px`, marginRight: `${m}px`, backgroundColor: `#${bgColor}`}}>
             
         </div>
     )
