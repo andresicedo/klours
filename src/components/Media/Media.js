be4f8ce42665e1836a158727c91b9bf4b5c89ed9
Andres Icedo <andresicedo24@gmail.com>
2021-10-03 21:07:23 -0500

    updated contact media component

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -3,8 +3,26 @@ import './Media.css';
 
 export default function Media() {
     return (
-        <div id="contact">
-            Social Media Component
+        <div id="contact" className="mediaContainer">
+            <div className="mediaLeft">
+                <div style={{display: 'flex', flexDirection: 'row'}}>
+                    <p>Contact Us</p>
+                    <div>
+                        <p>Store #1</p>
+                        <p>Street</p>
+                        <p>Houston, TX 11111</p>
+                        <p>111-111-1111</p>
+                    </div>
+                </div>
+            </div>
+            <div className="mediaRight">
+                <p>Find Us On</p>
+                <div>Facebook</div>
+                <div>Instagram</div>
+                <div>LinkedIn</div>
+                <div>Twitter</div>
+                <div>Email</div>
+            </div>
         </div>
     )
 }
